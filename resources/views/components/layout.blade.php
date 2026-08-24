be4edcb7010a305f6a83bdc40f2342972fb19087
Anjalika Rukshani <anjalikarukshani51@gmail.com>
2026-08-24 17:12:30 +0530

    feat: add axios for HTTP requests in bootstrap file

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -1,33 +1,16 @@
-@props(['title' => 'Laracasts'])
+@props([
+    'title' => 'Laracasts'])
 
 <!doctype html>
-<html lang="en" data-theme="night">
+<html lang="en" data-theme="dracula">
 <head>
     <meta charset="UTF-8">
-
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
     <title>{{ $title }}</title>
-
-    <!-- Tailwind CSS -->
-    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
-
-    <!-- DaisyUI -->
-    <link
-        href="https://cdn.jsdelivr.net/npm/daisyui@5"
-        rel="stylesheet"
-        type="text/css"
-    />
-
-    <!-- DaisyUI Themes -->
-    <link
-        href="https://cdn.jsdelivr.net/npm/daisyui@5/themes.css"
-        rel="stylesheet"
-        type="text/css"
-    />
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
-<body class="min-h-screen bg-base-200">
+<body class="text-primary">
 
     <!-- Navigation -->
     <x-nav />
